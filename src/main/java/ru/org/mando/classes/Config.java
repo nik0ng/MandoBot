d9f49c66bb90da74e49790004e24e1ba3c6aed3e
alexeynikanorov <67226503+nik0ng@users.noreply.github.com>
2023-08-08 09:52:35 +0300

    fix(main): add Marat in admins list

--- src/main/java/ru/org/mando/classes/Config.java
+++ src/main/java/ru/org/mando/classes/Config.java
@@ -7,15 +7,16 @@ import java.util.List;
 @Component
 public class Config {
 
-//    public static Boolean isWork = true;
     private static final String BOT_TOKEN = "6201461209:AAFbCcYZhQgK_aX0n7CfQhUEOmyBKnXidQM";
+    // admins
     private static final long NIK0NG = 567461715;
+    private static final long MARAT = 594333443;
+    private static final List<Long> USERS_TO_SEND_NOTIFICATION = List.of(NIK0NG, MARAT);
+
     private static final double THRESHOLD = 90.0;
     //test
 //    private static final double THRESHOLD = 50.0;
 
-
-    private static final List<Long> USERS_TO_SEND_NOTIFICATION = List.of(NIK0NG);
 
     public static String getBotToken(){
         return BOT_TOKEN;
@@ -28,10 +29,5 @@ public class Config {
     public static List<Long> getUsersToSendNotification(){
         return USERS_TO_SEND_NOTIFICATION;
     }
-
-//    public static void setIsWork(Boolean bool){
-//        isWork = bool;
-//    }
-
 
 }
